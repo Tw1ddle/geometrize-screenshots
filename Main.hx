85db7c48a26fa444f7292623ca24c7d61eea84b7
sam <sam@silver42.karoo.co.uk>
2017-09-13 22:25:40 +0100

    Add screenshots to page... could replace js with macros/template system later

--- Main.hx
+++ Main.hx
@@ -1,6 +1,8 @@
 package;
 
 import js.Browser;
+import js.html.DivElement;
+import js.html.Headers;
 
 /**
  * Creates Javascript code for a webpage containing collections of screenshots
@@ -13,6 +15,7 @@ class Main {
 	private static inline function getElement(id:String):Dynamic {
 		return Browser.document.getElementById(id);
 	}
+	private static var imageContainer:DivElement = getElement("screenshot_container");
 
 	private static function main():Void {
 		var main = new Main();
@@ -23,7 +26,45 @@ class Main {
 	}
 	
 	private inline function init():Void {
-		var
+		var rootDir:String = "assets/images";
+		var geometrizeVersion:String = "geometrize_1_0"; // Use the Geometrize 1.0 screenshots folder. Will add new ones when bumping the Geometrize version for official releases
+		var basePath = rootDir + "/" + geometrizeVersion + "/";
+		
+		var filePaths = [
+			"launch_window", "launch_window_file_menu", "launch_window_view_menu", "launch_window_help_menu",
+			
+			"launch_window_open_image_dialog", "launch_window_download_webpage_dialog", "launch_window_run_script_dialog",
+			
+			"global_settings_load_dialog", "global_settings_save_dialog",
+			
+			"about_dialog", "credits_dialog", "license_dialog",
+			
+			"image_task_window_with_task_settings", "image_task_window_with_exporters", "image_task_load_settings_dialog", "image_task_save_settings_dialog", "image_task_file_menu", "image_task_view_menu",
+			
+			"exporter_save_image_dialog", "exporter_save_images_dialog", "exporter_save_svg_dialog", "exporter_save_gif_dialog", "exporter_save_HTML5_webpage_dialog", "exporter_save_WebGL_webpage_dialog", "exporter_save_geometry_data_dialog",
+			
+			"global_preferences_general_section", "global_preferences_display_section", "global_preferences_performance_section", "global_preferences_language_section",
+			
+			"script_editor_window"
+		];
+		
+		for (filePath in filePaths) {
+			appendItem(filePath, basePath + filePath + ".png");
+		}
+	}
+	
+	private inline function appendItem(fileHeader:String, filePath:String):Void {
+		var titleElement = js.Browser.document.createElement("h2");
+		titleElement.textContent = fileHeader;
+		
+		var imageElement = js.Browser.document.createImageElement();
+		imageElement.src = filePath;
+		
+		imageContainer.appendChild(Browser.document.createBRElement());
+		imageContainer.appendChild(titleElement);
+		imageContainer.appendChild(Browser.document.createBRElement());
+		imageContainer.appendChild(imageElement);
+		imageContainer.appendChild(Browser.document.createBRElement());
 	}
 
 	private inline function onWindowLoaded():Void {
--- docs/js/geometrize.js
+++ docs/js/geometrize.js
@@ -8,9 +8,28 @@ Main.main = function() {
 };
 Main.prototype = {
 	onWindowLoaded: function() {
+		var rootDir = "assets/images";
+		var geometrizeVersion = "geometrize_1_0";
+		var basePath = rootDir + "/" + geometrizeVersion + "/";
+		var filePaths = ["launch_window","launch_window_file_menu","launch_window_view_menu","launch_window_help_menu","launch_window_open_image_dialog","launch_window_download_webpage_dialog","launch_window_run_script_dialog","global_settings_load_dialog","global_settings_save_dialog","about_dialog","credits_dialog","license_dialog","image_task_window_with_task_settings","image_task_window_with_exporters","image_task_load_settings_dialog","image_task_save_settings_dialog","image_task_file_menu","image_task_view_menu","exporter_save_image_dialog","exporter_save_images_dialog","exporter_save_svg_dialog","exporter_save_gif_dialog","exporter_save_HTML5_webpage_dialog","exporter_save_WebGL_webpage_dialog","exporter_save_geometry_data_dialog","global_preferences_general_section","global_preferences_display_section","global_preferences_performance_section","global_preferences_language_section","script_editor_window"];
+		var _g = 0;
+		while(_g < filePaths.length) {
+			var filePath = filePaths[_g];
+			++_g;
+			var titleElement = window.document.createElement("h2");
+			titleElement.textContent = filePath;
+			var imageElement = window.document.createElement("img");
+			imageElement.src = basePath + filePath + ".png";
+			Main.imageContainer.appendChild(window.document.createElement("br"));
+			Main.imageContainer.appendChild(titleElement);
+			Main.imageContainer.appendChild(window.document.createElement("br"));
+			Main.imageContainer.appendChild(imageElement);
+			Main.imageContainer.appendChild(window.document.createElement("br"));
+		}
 	}
 };
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+Main.imageContainer = window.document.getElementById("screenshot_container");
 Main.main();
 })();
